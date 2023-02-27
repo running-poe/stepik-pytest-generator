c93c74287f570418da2b570a9eea87497586161f
Runningpoe <52130043+running-poe@users.noreply.github.com>
2023-02-27 19:29:04 +0300

    добавление методов

--- pytest_gen.py
+++ pytest_gen.py
@@ -1,14 +1,18 @@
 import yaml
 
-def init_configuration(fname='init.yaml') -> bool:
+def init_configuration(fname='init.yaml') -> {}:
     configuration = {
-        "input_data": "input.txt",
-        "output_data": "output.txt",
+        "input_test": "input.txt",
+        "results": "output.txt",
         "method": "func"
     }
-
-    with open(fname, "w") as f:
-        yaml.dump(configuration, f)
+    try:
+        with open(fname, "w") as f:
+            yaml.dump(configuration, f)
+    except:
+        return None
+    finally:
+        return configuration
 
 
 def get_configuration(fname='init.yaml') -> {}:
@@ -16,16 +20,40 @@ def get_configuration(fname='init.yaml') -> {}:
     try:
         with open(fname, "r") as f:
             configuration = yaml.load(f, Loader=yaml.FullLoader)
-    finally:
-        return configuration
+            return configuration
+    except:
+        return None
 
 
-def parse_input(p_in: []) -> []:
-    pass
+class StepikTestGenerator:
+
+    # читаем из файлов
+    def __init__(self, fname_input_test, fname_results, method):
+        with open(fname_input_test, 'r') as f:
+            self.__stream_input_test = f.readlines()
+
+        with open(fname_results, 'r') as f:
+            self.__stream_results = f.readlines()
+        self.__method = method
+
+    # читаем из файлов изначально через объект настроек
+    def __init__(self, configuration):
+        fname_input_test = configuration.get("input_test")
+        fname_results = configuration.get("results")
+        method = configuration.get("method")
+        self.__init__(fname_input_test, fname_results, method)
+
+    # читаем из потоков (строк)
+    def __init__(self, stream_input_test, stream_results, method):
+        self.__stream_input_test = stream_input_test
+        self.__stream_results = stream_results
+        self.__method = method
 
 
-def parse_input(fname: str) -> []:
-    pass
+    def doit(self):
+        pass
+
+
 
 
 
